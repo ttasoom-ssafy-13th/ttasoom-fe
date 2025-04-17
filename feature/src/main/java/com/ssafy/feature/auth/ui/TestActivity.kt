ca34c8a7a903e024a3dd99ff2c59cb587690f596
SSSSSungjun <SSSSSungjun@github.com>
2025-04-17 17:48:04 +0900

    feat : 보일러 조회 domain, di , data layer

--- feature/src/main/java/com/ssafy/feature/auth/ui/TestActivity.kt
+++ feature/src/main/java/com/ssafy/feature/auth/ui/TestActivity.kt
@@ -1,16 +1,9 @@
 package com.ssafy.feature.auth.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.ViewModel
-import com.ssafy.domain.model.Boiler
-import com.ssafy.feature.R
 import com.ssafy.feature.auth.viewmodel.TestViewModel
-import com.ssafy.feature.databinding.ActivityLoginBinding
 import com.ssafy.feature.databinding.ActivityTestBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -26,7 +19,7 @@ class TestActivity : AppCompatActivity() {
         setContentView(binding.root)
         super.onCreate(savedInstanceState)
 
-        val list= mutableListOf<Boiler>()
+       // val list= mutableListOf<Boiler>()
 
     }
 }
